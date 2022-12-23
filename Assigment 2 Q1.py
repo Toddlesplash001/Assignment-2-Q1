03e5845e1c01b7f662efddf389c75fcf7270c7f0
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2022-12-23 09:04:30 +0530

    Add files via upload

---
+++ Assigment 2 Q1.py
@@ -0,0 +1,8 @@
+v = "Python is a case sensitive language"
+print(len(v))
+print(v[::-1])
+a = v[10:36]
+print(a)
+print(v.replace("a case sensitive language", "object orineted lanuage"))
+print(v.index("a"))
+print(v.strip())
